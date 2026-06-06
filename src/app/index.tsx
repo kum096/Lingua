c668b7474251478b702283b049611546a543518d
DevKnb <kumnelson84@gmail.com>
2026-06-06 15:24:20 +0100

    all go

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-xl text-indigo-600  text-center mt-25">Lingu</Text>
+      <Text className="text-xl text-indigo-600  text-center mt-25">Lingua</Text>
     </View>
   );
 }
